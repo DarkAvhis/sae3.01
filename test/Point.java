b92161905d66476feb571534368997f22345df60
Enzo <dumontenzo7@gmail.com>
2025-12-08 13:57:43 +0100

    LectureFichier marche en vif

--- test/Point.java
+++ test/Point.java
@@ -12,16 +12,34 @@ public class Point
         this.y = y;
     }
 
-    public String getNom() {return nom;}
+    public String getNom() 
+    {
+        return nom;
+    }
 
-    public void setNom(String nom) {this.nom = nom;}
+    public void setNom(String nom) 
+    {
+        this.nom = nom;
+    }
 
-    public int getX() {return x;}
+    public int getX() 
+    {
+        return x;
+    }
 
-    public void setX(int x) {this.x = x;}
+    public void setX(int x) 
+    {
+        this.x = x;
+    }
 
-    public int getY() {return y;}
+    public int getY() 
+    {
+        return y;
+    }
 
-    public void setY(int y) {this.y = y;}
+    public void setY(int y) 
+    {
+        this.y = y;
+    }
 
 }
