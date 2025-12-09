31380c7f1d92b476a57964e8e4d5fe2069427877
Ariukaanaa <ariunbayar443@gmail.com>
2025-12-09 13:51:30 +0100

    Controleur

--- src/Controleur.java
+++ src/Controleur.java
@@ -1,3 +1,20 @@
-public class Controleur {
-	
+import modele.AnalyseurUML;
+import vue.FenetrePrincipale;
+
+public class Controleur 
+{
+	private FenetrePrincipale fenetre ; 
+	private AnalyseurUML      analyse;
+
+	public Controleur()
+	{
+		this.fenetre = new FenetrePrincipale(); 
+		this.analyse = new AnalyseurUML();
+		this.fenetre.setVisible(true);
+	}
+
+	public static void main(String[] args) 
+	{
+		new Controleur(); 
+	}
 }
