7271497e0604b4bae482b8e99bad999e7842864b
QuentinMorvan03 <morvan.quentin00@gmail.com>
2025-12-09 15:40:49 +0100

    InterfaceObjet javadoc

--- src/modele/InterfaceObjet.java
+++ src/modele/InterfaceObjet.java
@@ -1,17 +1,35 @@
 package modele;
 
+
+/**
+ * Représente une relation d'implémentation d'une interface dans un diagramme UML.
+ * Dans cette relation :
+ * classeDest : la classe qui implémente l'interface (la classe concrète)</li>
+ * classeOrig : l'interface ou classe abstraite implémentée</li>
+ */
 public class InterfaceObjet extends LiaisonObjet 
 {
-
-    public InterfaceObjet(ClasseObjet classeDest, ClasseObjet classeOrig, MultipliciteObjet multDest, MultipliciteObjet multOrig, String nomAttribut, boolean unidirectionnel) 
+     /**
+     * Crée une relation d'implémentation entre une classe et une interface.
+     *
+     * @param nomAttribut      Le nom de la relation (souvent vide pour l'implémentation)
+     * @param classeDest       La classe concrète qui implémente l'interface
+     * @param classeOrig       L'interface ou classe abstraite implémentée
+     */
+    public InterfaceObjet( String nomAttribut, ClasseObjet classeMere, ClasseObjet classeFille) 
     {
-        super(nomAttribut, classeDest, classeOrig);
+        super(nomAttribut, classeMere, classeFille);
     }
 
+    /**
+     * Retourne une représentation textuelle de la relation d'implémentation.
+     *
+     * @return une chaîne du type : "ClasseDest implémente ClasseOrig"
+     */
     @Override
     public String toString() 
     {
-        return classeDest.getNom() + "\timplémente " + classeOrig.getNom();
+        return classeMere.getNom() + "\timplémente " + classeFille.getNom();
     }
      
 }
