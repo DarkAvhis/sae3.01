f0d57fa1c0d0faa8ff733e15aee0d4320e4502da
Enzo <dumontenzo7@gmail.com>
2025-12-18 10:48:02 +0100

    correctif ihm ( modif associations + utilisation methode optimiser positions)

--- retroconcepteur/src/vue/EditeurLiaisonDialog.java
+++ retroconcepteur/src/vue/EditeurLiaisonDialog.java
@@ -2,6 +2,7 @@ package vue;
 
 import java.awt.BorderLayout;
 import java.awt.GridLayout;
+import java.awt.Frame;
 import java.awt.event.ActionEvent;
 import javax.swing.JButton;
 import javax.swing.JDialog;
@@ -12,19 +13,20 @@ import javax.swing.JTextField;
 /**
  * Dialog simple pour éditer une liaison (multiplicités, rôles et offsets).
  */
-public class EditeurLiaisonDialog extends JDialog {
+public class EditeurLiaisonDialog extends JDialog 
+{
 	private final LiaisonVue liaison;
 
 	private final JTextField tfMultOrig = new JTextField(10);
 	private final JTextField tfMultDest = new JTextField(10);
 	private final JTextField tfRoleOrig = new JTextField(12);
 	private final JTextField tfRoleDest = new JTextField(12);
-	private final JTextField tfOrigAlong = new JTextField(4);
-	private final JTextField tfOrigPerp = new JTextField(4);
-	private final JTextField tfDestAlong = new JTextField(4);
-	private final JTextField tfDestPerp = new JTextField(4);
 
-	public EditeurLiaisonDialog(java.awt.Frame owner, LiaisonVue liaison) {
+	private JButton btnOK;
+	private JButton btnCancel;
+
+	public EditeurLiaisonDialog(Frame owner, LiaisonVue liaison) 
+	{
 		super(owner, "Éditer la liaison", true);
 		this.liaison = liaison;
 
@@ -32,61 +34,36 @@ public class EditeurLiaisonDialog extends JDialog {
 		tfMultDest.setText(liaison.getMultipliciteDest());
 		tfRoleOrig.setText(liaison.getRoleOrig());
 		tfRoleDest.setText(liaison.getRoleDest());
-		tfOrigAlong.setText(String.valueOf(liaison.getRoleOrigOffsetAlong()));
-		tfOrigPerp.setText(String.valueOf(liaison.getRoleOrigOffsetPerp()));
-		tfDestAlong.setText(String.valueOf(liaison.getRoleDestOffsetAlong()));
-		tfDestPerp.setText(String.valueOf(liaison.getRoleDestOffsetPerp()));
 
-		JPanel form = new JPanel(new GridLayout(0, 2, 6, 6));
-		form.add(new JLabel("Multiplicité origine:"));
-		form.add(tfMultOrig);
-		form.add(new JLabel("Multiplicité destination:"));
-		form.add(tfMultDest);
-		form.add(new JLabel("Rôle origine:"));
-		form.add(tfRoleOrig);
-		form.add(new JLabel("Rôle destination:"));
-		form.add(tfRoleDest);
-		form.add(new JLabel("Orig offset (le long):"));
-		form.add(tfOrigAlong);
-		form.add(new JLabel("Orig offset (perp):"));
-		form.add(tfOrigPerp);
-		form.add(new JLabel("Dest offset (le q long):"));
-		form.add(tfDestAlong);
-		form.add(new JLabel("Dest offset (perp):"));
-		form.add(tfDestPerp);
+		JPanel panelDisposition = new JPanel(new GridLayout(0, 2, 6, 6));
+		panelDisposition.add(new JLabel("Multiplicité origine:"    ));
+		panelDisposition.add(tfMultOrig);
+		panelDisposition.add(new JLabel("Multiplicité destination:"));
+		panelDisposition.add(tfMultDest);
+		panelDisposition.add(new JLabel("Rôle origine:"            ));
+		panelDisposition.add(tfRoleOrig);
+		panelDisposition.add(new JLabel("Rôle destination:"        ));
+		panelDisposition.add(tfRoleDest);
 
-		JButton btnOK = new JButton("OK");
-		btnOK.addActionListener((ActionEvent e) -> {
-			liaison.setRoleOrig(tfRoleOrig.getText());
-			liaison.setRoleDest(tfRoleDest.getText());
-			liaison.setRoleOrigOffsetAlong(parseIntSafe(tfOrigAlong.getText(), liaison.getRoleOrigOffsetAlong()));
-			liaison.setRoleOrigOffsetPerp(parseIntSafe(tfOrigPerp.getText(), liaison.getRoleOrigOffsetPerp()));
-			liaison.setRoleDestOffsetAlong(parseIntSafe(tfDestAlong.getText(), liaison.getRoleDestOffsetAlong()));
-			liaison.setRoleDestOffsetPerp(parseIntSafe(tfDestPerp.getText(), liaison.getRoleDestOffsetPerp()));
+		this.btnOK = new JButton("OK");
+		this.btnOK.addActionListener((ActionEvent e) -> {
+			liaison.setRoleOrig(tfRoleOrig.getText()        );
+			liaison.setRoleDest(tfRoleDest.getText()        );
 			// Multiplicités
 			liaison.setMultipliciteOrig(tfMultOrig.getText());
 			liaison.setMultipliciteDest(tfMultDest.getText());
 			dispose();
 		});
 
-		JButton btnCancel = new JButton("Annuler");
-		btnCancel.addActionListener((ActionEvent e) -> dispose());
+		this.btnCancel = new JButton("Annuler");
+		this.btnCancel.addActionListener((ActionEvent e) -> dispose());
 
-		JPanel actions = new JPanel();
-		actions.add(btnOK);
-		actions.add(btnCancel);
-
-		getContentPane().add(form, BorderLayout.CENTER);
-		getContentPane().add(actions, BorderLayout.SOUTH);
+		JPanel panelAction = new JPanel();
+		panelAction.add(this.btnOK);
+		panelAction.add(this.btnCancel);
+		getContentPane().add(panelDisposition, BorderLayout.CENTER);
+		getContentPane().add(panelAction, BorderLayout.SOUTH);
 		pack();
 		setLocationRelativeTo(owner);
-	}
-
-	private int parseIntSafe(String s, int def) {
-		try {
-			return Integer.parseInt(s.trim());
-		} catch (Exception ex) {
-			return def;
-		}
 	}
 }
